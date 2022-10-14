1f7bf64ac8b3e5614a27398e6a80f7dd62cfc0a1
ayeebiswajit <91446639+biswajitpat@users.noreply.github.com>
2022-10-14 15:23:03 +0530

    First commit
    First commit added the hello World

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+// This is a simple pull request for the hactoberfest
+#include<stdio.h>
+int main(){
+printf("Hello World");
+printf("This is a Simple Contribution in the hactoberfest");
+printf("This is Biswajit pat A enthusiastic coder ");
+return 0;
+}
