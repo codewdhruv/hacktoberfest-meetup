fd1b5eedc2c4474571b918c9d8452b1cc740b352
Souvik Halder <souvik.halder2020@gift.edu.in>
2023-01-02 17:59:14 +0000

    Tasks in hardhat

--- blockchaincode/hardhat-simple-storage/hardhat.config.js
+++ blockchaincode/hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config()
-require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-etherscan');
+require('./tasks/block-number')
 const GORELI_RPC_URL=process.env.GORELI_RPC_URL;
 const PRIVATE_KEY=process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY=process.env.ETHERSCAN_API_KEY;
